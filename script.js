596c90e947421bbf0b76c492fa2bfe2dde3d1ab8
Frank <51930059+fxl4@users.noreply.github.com>
2023-07-05 22:18:42 -0400

    Fully functional PokeAPI.
    I should clean up the styling since it's quite boring at the moment.

--- script.js
+++ script.js
@@ -2,38 +2,27 @@
 let button = document.querySelector("#searchButton");
 let pokemonName = document.querySelector("#pokemonName");
 let pokemonImage = document.querySelector("#pokemonImage");
-let pokemonURL;
+
 
 // Event handlers
 function getPokemon() {
     const input = document.querySelector("#inputBar").value;
-    
     axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`)
         .then(function (response) {
+            // Successful response
             console.log(response);
-            pokemonName = response.data.forms.name;
-            console.log(pokemonName);
-            pokemonURL = response.data.forms.url;      
+            returnPokemonName = response.data.name;
+            console.log(returnPokemonName);
+            returnPokemonImage = response.data.sprites.front_default;
             // Update the DOM
-            document.querySelector("#pokemonName").innerText = `${pokemonName}`;           
+            document.querySelector("#pokemonName").innerText = `${returnPokemonName}`;
+            document.querySelector("#pokemonImage").src = `${returnPokemonImage}`;              
         })
         .catch(function (error) {
             // Error response
             console.log(error);
         });
-    };
-
-    axios.get(`${pokemonURL}`)
-        .then(function (response) {
-            console.log(response);
-            pokemonImage = response.data.sprites.front_default;
-            document.querySelector("#pokemonImage").innerText = `${pokemonImage}`; 
-        })
-        .catch(function (error) {
-            // Error message
-            console.log(error);
-        });
-    }
+};
 
 
 // Event listeners
